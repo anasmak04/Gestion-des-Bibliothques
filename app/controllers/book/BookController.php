94eec1b17505facc0fc03d76954fe8c89e450f6c
anasmak04 <anasdev8@gmail.com>
2023-12-18 21:50:34 +0100

    Implemented a feature to save book information into the database

--- app/controllers/book/BookController.php
+++ app/controllers/book/BookController.php
@@ -3,14 +3,53 @@
 namespace Book;
 
 use dao\DaoInterface;
-require_once "../../dao/DaoInterface.php";
+use DbConfig;
+use models\Book;
+use models\User;
 
+require_once "../../dao/DaoInterface.php";
+require_once "../../models/Book.php";
+require_once "../../database/DbConfig.php";
 class BookController implements DaoInterface
 {
 
-    public function save($entity)
+
+        private $databasse;
+
+    /**
+     * @param $databasse
+     */
+    public function __construct()
     {
-        // TODO: Implement save() method.
+        $db = new DbConfig();
+        $this->databasse = $db->getConnection();
+    }
+
+
+    public function save($Book)
+    {
+
+     $sql = ("INSERT INTO `book`(`title`, `author`, `genre`, `description`, `publication_year`, `total_copies`, `available_copies`) VALUES (:title,:author,:genre,:description,:publication_year,:total_copies,:available_copies)");
+
+     $statement = $this->databasse->prepare($sql);
+
+     $title = $Book->getTitle();
+     $author = $Book->getAuthor();
+     $genre = $Book->getGenre();
+     $description = $Book->getDescription();
+     $publication_year = $Book->getPublicationYear();
+     $total_copies = $Book->getTotalCopies();
+     $available_copies = $Book->getAvailableCopies();
+
+     $statement->bindParam(':title', $title);
+     $statement->bindParam(':author', $author);
+     $statement->bindParam(':genre', $genre);
+     $statement->bindParam(':description', $description);
+     $statement->bindParam(':publication_year', $publication_year);
+     $statement->bindParam(':total_copies', $total_copies);
+     $statement->bindParam(':available_copies', $available_copies);
+     $statement->execute();
+
     }
 
     public function update($entity)
@@ -32,4 +71,18 @@ class BookController implements DaoInterface
     {
         // TODO: Implement deleteById() method.
     }
+
+}
+
+if(isset($_POST['submit'])) {
+    $user = new Book(null,null,null,null,null,null,null);
+    $user->setTitle($_POST['title']);
+    $user->setAuthor($_POST['author']);
+    $user->setGenre($_POST['genre']);
+    $user->setDescription($_POST['description']);
+    $user->setPublicationYear($_POST['publication_year']);
+    $user->setTotalCopies($_POST['total_copies']);
+    $user->setAvailableCopies($_POST['available_copies']);
+    $bookimp = new BookController();
+    $bookimp->save($user);
 }
---
+++ views/book/book.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Books Management">
+    <meta name="keywords" content="Library books">
+    <meta name="author" content="Anas Elmakhloufi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../public/css/styles.css">
+
+</head>
+
+<body>
+<h2>Login</h2>
+
+<form action="../../app/controllers/Book/BookController.php" method="post">
+
+    <div class="form-group">
+        <label for="title">title:</label>
+        <input name="title" type="text" class="form-control"  id="title" required>
+    </div>
+
+    <div class="form-group">
+        <label for="author">author:</label>
+        <input name="author" type="text" class="form-control"  id="author" required>
+    </div>
+
+    <div class="form-group">
+        <label for="genre">genre:</label>
+        <input name="genre" type="text" class="form-control"  id="genre" required>
+    </div>
+
+    <div class="form-group">
+        <label for="description">description:</label>
+        <input name="description" type="text" class="form-control"  id="description" required>
+    </div>
+
+
+    <div class="form-group">
+        <label for="publication_year">publication_year:</label>
+        <input name="publication_year" type="date" class="form-control"  id="publication_year" required>
+    </div>
+
+    <div class="form-group">
+        <label for="total_copies">total_copies:</label>
+        <input name="total_copies" type="number" class="form-control"  id="total_copies" required>
+    </div>
+
+    <div class="form-group">
+        <label for="available_copies">available_copies:</label>
+        <input name="available_copies" type="number" class="form-control"  id="available_copies" required>
+    </div>
+
+
+    <button name="submit" type="submit" class="btn btn-success">Register</button>
+</form>
+</body>
+</html>
